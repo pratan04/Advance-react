11473d76de04bdd046e536e975f8a7dd61c48dfd
pratan04 <prashantt.ratan04@gmail.com>
2024-04-05 00:16:08 +0530

    adding initial files

---
+++ src/components/PostList.jsx
@@ -0,0 +1,86 @@
+
+import { useContext, useEffect, useState } from "react";
+import Post from "./Post";
+import  PostListProvider, {PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+
+
+const PostList = ()=>{
+
+        const {postList,addInitialPosts} = useContext(PostListData);
+
+        const [fetching,setFetching]=useState(false);
+
+    useEffect(()=>{
+        setFetching(true);
+        
+        const controller=new AbortController();
+        const signal=controller.signal;
+
+
+        fetch('https://dummyjson.com/posts',{signal})
+        .then(res => res.json())
+        
+        .then(data =>{addInitialPosts(data.posts)
+            
+            setFetching(false)
+        });
+        
+        return ()=>{ console.log("cleanup in progress")
+        controller.abort();
+    }
+    },[])
+
+
+
+
+
+
+
+
+
+    //    const [data,setData]=useState(false);
+
+    //    if(!data){
+
+    //     fetch('https://dummyjson.com/posts')
+    //     .then(res => res.json())
+    //     .then(data =>{addInitialPosts(data.posts)});
+    //     setData(true);
+    //    }
+    
+
+        const handleGetPostsClick=()=>{
+            // fetch('https://dummyjson.com/posts')
+            // .then(res => res.json())
+            // .then(data =>{addInitialPosts(data.posts)});
+            
+
+            
+
+        }
+
+        
+
+
+    return (<div>
+
+        {fetching && <LoadingSpinner/>}
+
+    {!fetching && postList.length===0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick}/>}
+
+    {!fetching && postList.map((post)=>(<Post key={post.id} post={post} />))}
+    </div>)
+
+
+    // <Post/>
+    // <Post/>
+    // <Post/>
+    // <Post/>
+    // <Post/> 
+
+    
+}
+export default PostList;
